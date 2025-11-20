b198e08ba76438c2899c2e7ed1732bdb9755aded
amoumouni <ariannakelleylomax@gmail.com>
2025-11-20 15:40:43 -0800

    Remove secret from repo

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
 // This is your test secret API key.
-const stripe = require('stripe')('sk_test_51SESF9Fiigk3meIYUYfRrKRqB3cGXkVY6ZvtlCTYApd88LtcmP7m3bCLGTpzivsbCSlguhp63fvuM9YLTNdOEavO00gkFNd7dq');
-const express = require('express');
+const API_KEY = process.env.MY_API_KEY;
+const express = require("express");
 const app = express();
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const YOUR_DOMAIN = "http://localhost:4242";
 
-app.post('/create-checkout-session', async (req, res) => {
+app.post("/create-checkout-session", async (req, res) => {
   const prices = await stripe.prices.list({
     lookup_keys: [req.body.lookup_key],
-    expand: ['data.product'],
+    expand: ["data.product"],
   });
   const session = await stripe.checkout.sessions.create({
-    billing_address_collection: 'auto',
+    billing_address_collection: "auto",
     line_items: [
       {
         price: prices.data[0].id,
         // For usage-based billing, don't pass quantity
         quantity: 1,
-
       },
     ],
-    mode: 'subscription',
+    mode: "subscription",
     success_url: `${YOUR_DOMAIN}/success.html?session_id={CHECKOUT_SESSION_ID}`,
     cancel_url: `${YOUR_DOMAIN}/cancel.html`,
     subscription_data: {
@@ -38,7 +37,7 @@ app.post('/create-checkout-session', async (req, res) => {
   res.redirect(303, session.url);
 });
 
-app.post('/create-portal-session', async (req, res) => {
+app.post("/create-portal-session", async (req, res) => {
   // For demonstration purposes, we're using the Checkout session to retrieve the customer ID.
   // Typically this is stored alongside the authenticated user in your database.
   const { session_id } = req.body;
@@ -57,20 +56,20 @@ app.post('/create-portal-session', async (req, res) => {
 });
 
 app.post(
-  '/webhook',
-  express.raw({ type: 'application/json' }),
+  "/webhook",
+  express.raw({ type: "application/json" }),
   (request, response) => {
     let event = request.body;
     // Replace this endpoint secret with your endpoint's unique secret
     // If you are testing with the CLI, find the secret by running 'stripe listen'
     // If you are using an endpoint defined with the API or dashboard, look in your webhook settings
     // at https://dashboard.stripe.com/webhooks
-    const endpointSecret = 'whsec_12345';
+    const endpointSecret = "whsec_12345";
     // Only verify the event if you have an endpoint secret defined.
     // Otherwise use the basic event deserialized with JSON.parse
     if (endpointSecret) {
       // Get the signature sent by Stripe
-      const signature = request.headers['stripe-signature'];
+      const signature = request.headers["stripe-signature"];
       try {
         event = stripe.webhooks.constructEvent(
           request.body,
@@ -86,35 +85,35 @@ app.post(
     let status;
     // Handle the event
     switch (event.type) {
-      case 'customer.subscription.trial_will_end':
+      case "customer.subscription.trial_will_end":
         subscription = event.data.object;
         status = subscription.status;
         console.log(`Subscription status is ${status}.`);
         // Then define and call a method to handle the subscription trial ending.
         // handleSubscriptionTrialEnding(subscription);
         break;
-      case 'customer.subscription.deleted':
+      case "customer.subscription.deleted":
         subscription = event.data.object;
         status = subscription.status;
         console.log(`Subscription status is ${status}.`);
         // Then define and call a method to handle the subscription deleted.
         // handleSubscriptionDeleted(subscriptionDeleted);
         break;
-      case 'customer.subscription.created':
+      case "customer.subscription.created":
         subscription = event.data.object;
         status = subscription.status;
         console.log(`Subscription status is ${status}.`);
         // Then define and call a method to handle the subscription created.
         // handleSubscriptionCreated(subscription);
         break;
-      case 'customer.subscription.updated':
+      case "customer.subscription.updated":
         subscription = event.data.object;
         status = subscription.status;
         console.log(`Subscription status is ${status}.`);
         // Then define and call a method to handle the subscription update.
         // handleSubscriptionUpdated(subscription);
         break;
-      case 'entitlements.active_entitlement_summary.updated':
+      case "entitlements.active_entitlement_summary.updated":
         subscription = event.data.object;
         console.log(`Active entitlement summary updated for ${subscription}.`);
         // Then define and call a method to handle active entitlement summary updated
@@ -129,4 +128,4 @@ app.post(
   }
 );
 
-app.listen(4242, () => console.log('Running on port 4242'));
+app.listen(4242, () => console.log("Running on port 4242"));
